84823b2f7316775d593df881d80c5a2911058f63
Xu Jie Sng <sngxujie@gmail.com>
2024-03-25 03:50:22 +0000

    changes to categories api

--- routes/api/categories.js
+++ routes/api/categories.js
@@ -5,9 +5,14 @@ const productDataLayer = require("../../dal/products")
 
 router.get("/", async (req, res) => {
     let unSortedCategoriesData = await productDataLayer.getAllCategories()
-    const allCategories = unSortedCategoriesData.sort((a, b) => a[0] - b[0]);
-
-
+    unSortedCategoriesData = unSortedCategoriesData.sort((a, b) => a[0] - b[0]);
+    unSortedCategoriesData  = {
+        categories: unSortedCategoriesData.map(category => {
+            return { _id: category[0], name: category[1] };
+        })
+    };
+    JSON.stringify(unSortedCategoriesData, null, 2)
+    const allCategories = unSortedCategoriesData.categories
     res.json({
         "categories": allCategories
     })
